90a4446a7e12ebea77f051e9fb852d6c87f65156
Feif2140 <54783197+Feif2140@users.noreply.github.com>
2021-01-17 00:14:13 -0500

    First Version of website

---
+++ website/script.js
@@ -0,0 +1,29 @@
+function togglemode () {
+    document.body.classList.toggle("dark");
+}
+
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
+
+  window.onscroll = function() {myFunction()};
+
+// Get the header
+var header = document.getElementById("myTopnav");
+
+// Get the offset position of the navbar
+var sticky = header.offsetTop;
+
+// Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.pageYOffset > sticky) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
+}
